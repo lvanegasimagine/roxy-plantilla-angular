156ca705b967076ba46f876e3fecb9e9b29c7780
Luis Vanegas <lvanegas1429@gmail.com>
2021-12-05 21:12:17 -0600

    Paginas implementadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { SingleComponent } from './components/single/single.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'single', component: SingleComponent },
   { path: '', pathMatch: 'full', redirectTo: '' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
